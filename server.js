fb4ff73517bd8d80ed1d97ff20e5dc213c434054
sim-sal <simone.saladino93@gmail.com>
2023-11-08 00:19:39 +0100

    created file ".gitignore" and a copy of file ".env" for push

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // importiamo la libreria http tramite "require" che è l'alter-ego di "import"
 const http = require("http");
 
+// importiamo libreria dotenv
+const dotenv = require("dotenv");
+
+// avviamo dotenv
+dotenv.config();
+
 // creiamo una variabile per la porta da utilizzare, impostando un'alternativa nel caso in cui la porta è già occupata
 const port = process.env.PORT || 3000;
 
@@ -9,7 +15,10 @@ const server = http.createServer(function (req, res) {
 
     // specifichiamo il modo in cui il server deve rispondere
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Hello World!</h1>");
+    res.end(`<h1>Hello World!</h1>
+    NICKNAME: ${process.env.NICKNAME}<br>
+    PIN: ${process.env.PIN}
+    `);
 });
 
 // mettiamo il server in ascolto verso la porta da noi scelta mediante la funzione "listen" che avrà come primo parametro la porta sulla quale deve rimanere in ascolto e come secondo argomento la funzione che dovrà essere eseguita quando il server parte (il secondo parametro opzionale, se non specificato, è di base localhost)
